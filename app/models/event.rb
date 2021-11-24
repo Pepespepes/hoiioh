cc02d182cdd5052a68327e15aab6e8c384f5e73b
Pepe Baena <pepespepes@gmail.com>
2021-11-24 16:36:50 +0000

    map display geocoder

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,8 @@ class Event < ApplicationRecord
 
   has_many :bookings
 
-  validates :location, :date, :start_time, :duration, :description, :number_positions, presence: true
+  validates :address, :date, :start_time, :duration, :description, :number_positions, presence: true
   validates :duration, inclusion: { in: (1..8) }
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
