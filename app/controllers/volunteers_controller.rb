654f82f607c736e15f7008371c44acb954f1e9f5
Isabel Foo <isabel.foo@hotmail.com>
2021-11-24 14:53:28 +0000

    alternative sign ups for charities, volunteers and employers

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -3,13 +3,18 @@ class VolunteersController < ApplicationController
   def create
     @volunteer = Volunteer.new(volunteer_params)
     @volunteer.user = current_user
-    @volunteer.save
+    # raise
+    if @volunteer.save!
+      redirect_to root_path
+    else
+      redirect_to details_path
+    end
   end
 
   private
 
   def volunteer_params
-    params.require(:volunteer).permit(:first_name, :last_name, :employer_id, :description, :age, :dbs_checked)
+    params.require(:volunteer).permit(:first_name, :last_name, :description, :age, :dbs_checked, :employer_id)
   end
 
 end
