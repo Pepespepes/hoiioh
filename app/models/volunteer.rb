db07849f6aaa57b100c42a1610b84b8a234925dd
KatyKilpatrick <katy.kilpatrick@hotmail.co.uk>
2021-11-25 17:03:41 +0000

    updated

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,6 +1,7 @@
 class Volunteer < ApplicationRecord
   belongs_to :employer, optional: true
   belongs_to :user
+  has_many :bookings
 
   validates :first_name, presence: true
   validates :last_name, presence: true
