955e4598890ae2f6bff8d83188da5ef36c1e6839
Pepe Baena <pepespepes@gmail.com>
2021-11-25 12:36:33 +0000

    map search bar added & change on h tags

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from "@hotwired/stimulus";
 import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
 export default class extends Controller {
   static values = {
@@ -14,11 +15,17 @@ export default class extends Controller {
       style: 'mapbox://styles/mapbox/streets-v10'
     });
     // call fucntion here
+    this.map.addControl(new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl
+    }));
     this._addMarkersToMap();
   }
 
   //addmarkers to map here
   _addMarkersToMap() {
+     console.log("hello")
+    console.log(this.markersValue)
     this.markersValue.forEach((marker) => {
       new mapboxgl.Marker()
         .setLngLat([marker.lng, marker.lat])
