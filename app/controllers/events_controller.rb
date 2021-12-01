e02b68cd50d5f1459fa8a7155c697c54a9b74981
KatyKilpatrick <katy.kilpatrick@hotmail.co.uk>
2021-12-01 12:10:45 +0000

    chatroom

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -78,6 +78,7 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
+    @user = @event.charity.user.id
   end
 
   def event_params
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,11 +6,16 @@
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import {initChatroomCable} from "../channels/chatroom_channel"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
 import "controllers"
+import "channels"
 import "bootstrap"
+
+document.addEventListener('turbolinks:load', () => {
+  initChatroomCable();
+});
