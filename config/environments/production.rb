892936d9a3e3b2f9139f8a89cb2f0a1f44e089e3
KatyKilpatrick <katy.kilpatrick@hotmail.co.uk>
2021-12-02 09:35:57 +0000

    chatroom

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -45,7 +45,9 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.web_socket_server_url = "wss://hoiioh.com/cable"
 
+  config.action_cable.allowed_request_origins = ['https://hoiioh.com', 'http://hoiioh.com']
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
